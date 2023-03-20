ecb72b4ac5bc5cc09a3f822889bf732198daac4b
Zakaria Bouabidi <zakariabouabidi19@gmail.com>
2023-03-20 14:01:14 -0700

    Numberz task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
