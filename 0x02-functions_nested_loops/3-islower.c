c299226d5214d91e1dfda750089113ea82d5f7b0
Zakaria Bouabidi <zakariabouabidi19@gmail.com>
2023-03-26 09:20:29 -0700

    islower task fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet - a founction that print aphabet.
+ * _islower - a founction that check for lowercase charcter.
+ * @c: the argument of the function.
+ *
+ * Return: we return 1 if c is lowercase or return 0 if otherwise.
+ *
  */
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-	return (0);
+		return (0);
 	}
 	_putchar('\n');
 
