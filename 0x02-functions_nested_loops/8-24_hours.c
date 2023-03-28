1720ff3619c9bdfba405fa9de0286afe68d83e0a
Zakaria Bouabidi <zakariabouabidi19@gmail.com>
2023-03-28 06:51:47 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - a founction that prints every minute of the day.
+ */
+void jack_bauer(void)
+{
+	int m, M;
+	int h, H;
+
+	for (H = 0; H <= 2; H++)
+	{
+		for (h = 0; h <= 9; h++)
+		{
+			for (M = 0; M <= 5; M++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					ifi(H >= 2 && h >= 4)
+						break;
+					_putchar(H + 48);
+					_putchar(h + 48);
+					_putchar(38);
+					_putchar(M + 48);
+					_putchar(m + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
