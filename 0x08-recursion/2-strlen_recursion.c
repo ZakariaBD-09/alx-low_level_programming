46c34ceb7e19bcb72d148b1b3db1e0443cb15f52
Zakaria Bouabidi <zakariabouabidi19@gmail.com>
2023-04-05 17:11:08 -0700

    fix betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,16 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _strlen_recursion - a function that returns the length of a string using recursion.
+ * _strlen_recursion - return the length of a string using recursion.
  * @s: parameter from type char.
+ *
+ * Return: return the legth of a dtring.
  */
-
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-	return 1 + _strlen_recursion(s + 1); 
+	return (1 + _strlen_recursion(s + 1));
 }
