586158b1721a82f0854c74c4a4373d59c2033e1d
Zakaria Bouabidi <zakariabouabidi19@gmail.com>
2023-04-05 18:15:02 -0700

    fix betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,42 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
+ * _sqrt_recursion_temp - recursively checks if i squared is equal to n.
+ * @i: parameter from type int.
  * @n: parameter from type int.
  *
- * Return: return -1 if n equal 0 or 1.
+ * Return: return -1 if i squard is greater tha n.
  */
 int _sqrt_recursion_temp(int n, int i)
 {
-	if (i * i == n) {
+	if (i * i == n)
+	{
 		return (i);
-	} else if (i * i > n) {
+	}
+	else if (i * i > n)
+	{
 		return (-1);
-	} else {
+	}
+	else
+	{
 		return (_sqrt_recursion_temp(n, i + 1));
 	}
 }
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: parameter from type int.
+ *
+ *Return: return -1 if n equal 0 or 1.
+ */
+
 int _sqrt_recursion(int n)
 {
-	if (n < 0) {
+	if (n < 0)
 		return (-1);
-	}
-	if (n == 0 || n == 1) {
+
+	if (n == 0 || n == 1)
 		return (n);
-	}
+
 	return (_sqrt_recursion_temp(n, 1));
 }
