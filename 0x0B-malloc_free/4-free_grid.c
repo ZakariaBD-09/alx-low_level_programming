8af74806c561954ff5fca93d5c5560718b087347
Zakaria Bouabidi <zakariabouabidi19@gmail.com>
2023-04-11 14:41:57 -0700

    a function that frees a 2 dimensional grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - the function free grid;
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
