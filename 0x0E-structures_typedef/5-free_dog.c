aa3d7ddcbe319ec04c4f13f70386eda1cfde4b42
Zakaria Bouabidi <zakariabouabidi19@gmail.com>
2023-04-18 19:24:36 -0700

    a function that frees dog struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog -  a function that frees dog struct.
+ * @d: new struct.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+
+		free(d);
+	}
+}
